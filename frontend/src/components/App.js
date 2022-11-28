6d4f80558b9f5d4780fe18c39f355e48337739d8
johnmulvey <johnmulvey48@yahoo.com>
2022-11-28 13:33:30 -0500

    added backend models and database

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./Home";
-import Navbar from "./Navbar";
 
 function App() {
     return (
